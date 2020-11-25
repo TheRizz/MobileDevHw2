b8007a384ba57b393235956bccf94ed74c4ca38a
TheRizz <19941577+TheRizz@users.noreply.github.com>
2020-11-25 12:36:34 -0800

    Update Claim.kt

--- app/src/main/java/com/example/cpsc411homework2/Claim.kt
+++ app/src/main/java/com/example/cpsc411homework2/Claim.kt
@@ -3,8 +3,5 @@
 
 package com.example.cpsc411homework2
 
-
-// only needs title and date
-// REST server assigns UUID and isSolved boolean
 data class Claim (var title: String?, var date: String?){
 }
